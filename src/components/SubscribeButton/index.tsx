a52999d047e353b415ddc8a4f2ba17b839afac5a
Bruno Henrique <brunohenriquex00@gmail.com>
2023-04-29 21:31:13 -0300

    feat: gerenciando escrição

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,21 +1,42 @@
+import React from "react";
 
-import React from 'react';
-
-import styles from './styles.module.scss'
-
+import styles from "./styles.module.scss";
+import { useSession, signIn } from "next-auth/react";
+import { api } from '../../services/api'
+import { getStripeJs } from "@/services/stripe-js";
 interface SubscribeButtonProps {
-  priceId: string
+  priceId: string;
 }
 
 const SubscribeButton: React.FC<SubscribeButtonProps> = ({ priceId }) => {
+  const session = useSession();
+
+  const handleSubscribe = async () => {
+    if (!session) {
+      signIn('github');
+      return 
+    }
+
+    // criacao da checkkout session
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+      console.log('session id', sessionId)
+
+      const stripe = await getStripeJs();
+
+      await stripe?.redirectToCheckout({ sessionId })
+    } catch (error:any) {
+      alert(error.message)
+    }
+  };
+
   return (
-    <button
-        type='button'
-        className={styles.subscribeButton}
-    >
-        Subscrive now
+    <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
+      Subscrive now
     </button>
-  )
-}
+  );
+};
 
 export default SubscribeButton;
