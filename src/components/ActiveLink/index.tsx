3c1b0a880015e585d93cbec73fb7913273b608d5
Bruno Henrique <brunohenriquex00@gmail.com>
2023-05-01 15:05:39 -0300

    feat: criando componente para controlar o link ativo na pagina

---
+++ src/components/ActiveLink/index.tsx
@@ -0,0 +1,28 @@
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import React, { ReactElement, cloneElement } from "react";
+
+interface IActiveLinkProps extends LinkProps {
+  children: ReactElement;
+  activeClassName: string;
+}
+
+const ActiveLink: React.FC<IActiveLinkProps> = ({
+  children,
+  activeClassName,
+  ...rest
+}) => {
+  const { asPath } = useRouter();
+
+  const className = asPath === rest.href ? activeClassName : "";
+
+  return (
+    <Link {...rest}>
+      {cloneElement(children, {
+        className,
+      })}
+    </Link>
+  );
+};
+
+export default ActiveLink;
