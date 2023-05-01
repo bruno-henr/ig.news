6dd7584b0887e37ce7c01670b4313b23213742e2
Bruno Henrique <brunohenriquex00@gmail.com>
2023-05-01 15:06:05 -0300

    feat: utilzando o activelink e alterando styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,25 @@
 import React from "react";
+import Link from "next/link";
 
 import styles from "./styles.module.scss";
 import { SignInButton } from "../SignInButton";
-import { useSession } from 'next-auth/react'
+import ActiveLink from "../ActiveLink";
+
 export const Header: React.FC = () => {
+  
+  
   return (
     <>
       <header className={styles.headerContainer}>
         <div className={styles.headerContent}>
           <img src="/images/logo.svg" alt="ig.news" />
           <nav>
-            <a href="" className={styles.active}>
-              Home
-            </a>
-            <a href="">Posts</a>
+            <ActiveLink activeClassName={styles.active} href="/">
+              <span>Home</span>
+            </ActiveLink>
+            <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+              <span>Posts</span>
+            </ActiveLink>
           </nav>
 
           <SignInButton />
